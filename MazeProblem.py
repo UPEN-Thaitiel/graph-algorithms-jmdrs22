fa452358822b9a759fe2e010ab3ab4a84e95cf58
Julio Inda <indaj69@gmail.com>
2025-05-16 10:34:47 -0700

    tarea de jazmin

--- MazeProblem.py
+++ MazeProblem.py
@@ -37,39 +37,68 @@ Good luck!
 
 
 """
+def solve_maze(maze): #la funcion maze es la representacion del laberinto
+    rows, cols = len(maze), len(maze[0]) #row y cols, los numeros de filas y columnas
+    path = [] #aqui se guardan las coordenadas 
+
+    def dfs(r, c):
+        if r < 0 or r >= rows or c < 0 or c >= cols:
+            return False
+        if maze[r][c] == 0 or (r, c) in path:
+            return False
+        path.append((r, c))
+        if (r, c) == (rows - 1, cols - 1):
+            return True
+        if dfs(r + 1, c) or dfs(r, c + 1) or dfs(r - 1, c) or dfs(r, c - 1):
+            return True
+        path.pop()
+        return False
+
+    dfs(0, 0)
+
+    result = [['-' for _ in range(cols)] for _ in range(rows)]
+    for r, c in path:
+        result[r][c] = 'S'
+    return result
+
+
+def print_maze(maze):
+    for row in maze:
+        print(row)
+    print()
+
 
 if __name__ == '__main__':
-    ### Your code must succesfully solve the following mazes:
-    
-    m = [[1, 0, 0, 1],
+    mazes = [
+        [[1, 0, 0, 1],
          [1, 0, 0, 1],
          [1, 0, 0, 1],
-         [1, 1, 1, 1]
-         ]
+         [1, 1, 1, 1]],
 
-    easy_maze = [
-        [1, 1, 1, 0, 1],
-        [1, 0, 1, 0, 1],
-        [1, 0, 1, 1, 1],
-        [1, 1, 0, 0, 1],
-        [0, 1, 1, 1, 1]
+        [[1, 1, 1, 0, 1],
+         [1, 0, 1, 0, 1],
+         [1, 0, 1, 1, 1],
+         [1, 1, 0, 0, 1],
+         [0, 1, 1, 1, 1]],
+
+        [[1, 1, 0, 1, 1, 0],
+         [0, 1, 0, 1, 0, 1],
+         [1, 1, 1, 1, 1, 1],
+         [1, 0, 0, 0, 0, 0],
+         [1, 1, 1, 1, 1, 1],
+         [0, 0, 1, 0, 0, 1]],
+
+        [[1, 0, 1, 1, 1, 0, 1],
+         [1, 0, 1, 0, 1, 0, 1],
+         [1, 1, 1, 0, 1, 1, 1],
+         [0, 0, 1, 0, 0, 0, 0],
+         [1, 1, 1, 1, 1, 1, 1],
+         [1, 0, 0, 0, 0, 0, 1],
+         [1, 1, 1, 1, 1, 1, 1]]
     ]
 
-    medium_maze = [
-        [1, 1, 0, 1, 1, 0],
-        [0, 1, 0, 1, 0, 1],
-        [1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1],
-        [0, 0, 1, 0, 0, 1]
-    ]   
-    hard_maze = [
-        [1, 0, 1, 1, 1, 0, 1],
-        [1, 0, 1, 0, 1, 0, 1],
-        [1, 1, 1, 0, 1, 1, 1],
-        [0, 0, 1, 0, 0, 0, 0],
-        [1, 1, 1, 1, 1, 1, 1],
-        [1, 0, 0, 0, 0, 0, 1],
-        [1, 1, 1, 1, 1, 1, 1]
-    ]
+    for i, maze in enumerate(mazes):
+        print(f"Laberinto {i + 1}:")
+        solution = solve_maze(maze)
+        print_maze(solution)
 
